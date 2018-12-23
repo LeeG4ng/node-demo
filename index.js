62fce58306230dd6d82e296259d88b6e0f6f8d2c
LeeG4ng <LeeG4ng@qq.com>
2018-12-23 19:06:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');//导⼊入Express模块 
+var app = express();
+var https = require('https');
+var fs = require('fs');
+
+//设置监听端⼝ https 
+https.createServer({
+     key: fs.readFileSync('server.key'),
+     cert: fs.readFileSync('server.cert')
+   }, app).listen(8086, function() {
+     console.log("run 8086");
+ });
+app.get('/', function(req, res) {//设置 console.log('get root'); res.send('get root');
+});
